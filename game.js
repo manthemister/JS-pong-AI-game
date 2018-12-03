8bb35f9e3e571e479df029791eb61610f70dccac
manthemister <blockstar9000@gmail.com>
2018-12-03 11:38:50 -0800

    make

---
+++ game.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("game");
+var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+canvas.width = viewportWidth;
+var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+canvas.height = viewportHeight;
+var ctx = canvas.getContext("2d");
+function fighter(weapon1, weapon2){
+  
+}
